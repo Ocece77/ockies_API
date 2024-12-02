b73d9875df95c1a50aa7b8c54f4e9e53fc542574
océane <132077432+Ocece77@users.noreply.github.com>
2024-12-03 00:00:01 +0100

    Delete dao directory

--- dao/MessageDAO.kt
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-abstract class MessageDAO: JpaRepository<Message, Long>{}
--- dao/UtilisateurDAO.kt
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-abstract class UtilisateurDAO : JpaRepository<Media, Long> {}
