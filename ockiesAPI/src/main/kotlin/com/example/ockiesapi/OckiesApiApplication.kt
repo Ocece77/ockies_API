244d80cbf3ab7565c65e2841aac2fa28e05116b1
Ocece77 <oceanekasindupro@gmail.com>
2024-12-03 13:23:29 +0100

    recreate the project

---
+++ ockiesAPI/src/main/kotlin/com/example/ockiesapi/OckiesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ockiesapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OckiesApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<OckiesApiApplication>(*args)
+}
---
+++ ockiesAPI/src/test/kotlin/com/example/ockiesapi/OckiesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ockiesapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OckiesApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
